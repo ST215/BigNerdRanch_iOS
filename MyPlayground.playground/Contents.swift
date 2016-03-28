ac6b656ecaeb6fafe54119dab0eeaa9b1229bf6f
Stanley H Griggs II <shg5004@gmail.com>
2016-03-28 17:25:16 -0400

    Specifying Types

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,3 +5,10 @@ import UIKit
 var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
+
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
+var arrayOfInts: [Int]
+var dictionaryOfCapitalsByCountry: [String:String]
+var winningLotteryNummbers: Set<Int>
