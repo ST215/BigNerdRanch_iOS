c589419e2cd5ebcd951963b832349bc0879ca91c
Stanley H Griggs II <shg5004@gmail.com>
2016-03-29 12:02:21 -0400

    Literals and subscripting

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -12,3 +12,14 @@ var hasPet: Bool
 var arrayOfInts: [Int]
 var dictionaryOfCapitalsByCountry: [String:String]
 var winningLotteryNummbers: Set<Int>
+var str2 = "Hello, playground"
+str2 = "Hello, Swift"
+let constStr2 = str2
+
+let number = 42
+let fmStation = 91.1
+let countingUp = ["one", "two"]
+let nameNyParkingSpace = [13: "Alice", 27: "Bob"]
+let countingUp2 = ["one", "two"]
+let secondElement = countingUp2[1]
+
